6055db46de6854b1540cc58625c69dafb0bc9cc0
Dzmitry Novik <nowikmitya@yandex.ru>
2023-06-07 21:17:48 +0300

    job test #4

--- src/main/java/ru/novik/SpringAppToDeploy/controllers/MainController.java
+++ src/main/java/ru/novik/SpringAppToDeploy/controllers/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    String parameter = "World Wide WebBBBB";
+    String parameter = "World";
 
     @GetMapping("/")
     public String greeting(Model model) {
